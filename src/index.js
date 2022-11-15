7c9b13734a83054d6a3b2aaa3bcb93036b1bc9a4
marvin1604 <walter.rodriguez1992@gmail.com>
2022-11-14 23:47:21 -0500

    agregando funcionalidad a los botones de fechas

--- src/index.js
+++ src/index.js
@@ -14,44 +14,45 @@ const buttonDateTime = document.querySelector("#button-date-time");
 const fechaManual = document.querySelector("#fecha-manual");
 const stop = document.querySelector(".stop");
 
-
-const getRemainTime = deadline =>{
-    let now = new Date();
-    let remaintime = (new Date(deadline) - now + 1000)/1000;
-        remainSeconds = ('0' + Math.floor(remaintime % 60)).slice(-2);
-        remainMinutes = ('0' + Math.floor(remaintime / 60 % 60)).slice(-2);
-        remainHours = ('0' + Math.floor(remaintime / 3600 % 24)).slice(-2);
-        remainDays = ('0' + Math.floor(remaintime / (3600*24) % 30)).slice(-2);
-        remainMonth = ('0' + Math.floor(remaintime / (3600*24*30))).slice(-2);
-    return{
-        now,
-        remaintime,
-        remainSeconds,
-        remainMinutes,
-        remainHours,
-        remainDays,
-        remainMonth
+function counterTime(deadline, finalMessage){
+    const timeUpdate = setInterval(function(){
+    const now = new Date();
+    let remaintime = ((new Date(deadline) - now + 800)/1000);
+    if(remaintime <= 0){
+        remaintime += (3600*24*30*12);
     }
-}
+    const remainSeconds = ('0' + Math.floor(remaintime % 60)).slice(-2);
+    const remainMinutes = ('0' + Math.floor(remaintime / 60 % 60)).slice(-2);
+    const remainHours = ('0' + Math.floor(remaintime / 3600 % 24)).slice(-2);
+    const remainDays = ('0' + Math.floor(remaintime / (3600*24) % 30)).slice(-2);
+    const remainMonth = ('0' + Math.floor(remaintime / (3600*24*30))).slice(-2);
+    seconds.innerText = remainSeconds;
+    minutes.innerText = remainMinutes;
+    hours.innerText = remainHours;
+    days.innerText = remainDays;
+    month.innerText = remainMonth;
+    navidad.onclick = () =>{
+        clearInterval(timeUpdate);
+        console.log(deadline, finalMessage);
+    }
+    añoNuevo.onclick = () =>{
+        clearInterval(timeUpdate);
+        console.log(deadline, finalMessage);
+    }
+    buttonDateTime.onclick = () =>{
+        clearInterval(timeUpdate);
+        console.log(deadline, finalMessage);
+    }
 
-const countDown = (deadline, finalMessage) =>{    
-    const timeUpdate = setInterval(() =>{
-        let t = getRemainTime(deadline);
-        seconds.innerText = t.remainSeconds;
-        minutes.innerText = t.remainMinutes;
-        hours.innerText = t.remainHours;
-        days.innerText = t.remainDays;
-        month.innerText = t.remainMonth;
-        if(t.remaintime <= 0){
-            clearInterval(timeUpdate);
-            alert(finalMessage);
-            location.reload();
-        }
+    if(remaintime <= 0){
+        clearInterval(timeUpdate);
+        alert(finalMessage);
+        location.reload();
+    }
+
     },1000);
+    console.log(deadline);
 }
-
-
-
 function fecha(){
     setInterval(function(){
         let dias = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado","Domingo"];
@@ -60,48 +61,39 @@ function fecha(){
         horas = now.getHours();
         minutos = now.getMinutes();
         segundos = now.getSeconds();
-
         diaNumber = now.getDate();
         mes = now.getMonth();
         año = now.getFullYear();
-
         diaString= now.getDay()-1;
 
         fechaActual.innerText = dias[diaString] +" " +diaNumber+" de "+meses[mes]+" del "+año;
         horaActual.innerText = ("0"+horas).slice(-2) + " : "+ ("0" + minutos).slice(-2) + " : " + ("0" + segundos).slice(-2) ;
     },1000);
 }
-fecha()
+fecha();
 
 navidad.addEventListener("click", showNavidad);
 halloween.addEventListener("click", showHalloween);
 añoNuevo.addEventListener("click", showAñoNuevo);
 buttonDateTime.addEventListener("click", showDateTime);
-stop.addEventListener("click", reload )
-
+stop.addEventListener("click", reload );
 
 function showDateTime(){
-    let evento = dateTime.value;
-    console.log(evento);
-    countDown(evento,"Feliz dia, Llego el dia que tanto esperaste!!!");
+    counterTime(dateTime.value,"Feliz dia, Llego el dia que tanto esperaste!!!");
     fechaManual.reset();
 }
 
 function showNavidad() {
     let evento = navidad.value;
-    countDown(evento, "feliz Navidad");
-    // let foto = document.querySelector(".imagen").style = url("<img style='with:10%' src= 'https://sp-ao.shortpixel.ai/client/q_lqip,ret_wait,w_760,h_550/https://sumate.mx/wp-content/uploads/2021/12/Navidad-760x550.jpg'")
-    // foto.style.width="200px";
-    
+    counterTime(evento, "Feliz Navidad");
 }
-
 function showHalloween(){
     let evento = halloween.value;
-    countDown(evento, "feliz halloween");
+    counterTime(evento, "Feliz Halloween");
 }
 function showAñoNuevo(){
     let evento = añoNuevo.value;
-    countDown(evento, "Feliz Año Nuevo");
+    counterTime(evento, "Feliz Año Nuevo");
 }
 function reload(){
     location.reload();
